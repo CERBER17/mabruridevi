1922a42c96b9d24f8bbc085c2f81ac24230dd9fe
dewanakl <dewanakretarta29@gmail.com>
2025-07-19 18:50:00 +0700

    fix: double init

--- js/app/admin/scan.js
+++ js/app/admin/scan.js
@@ -1,30 +1,34 @@
 
 export const scan = (() => {
 
-    const x = async () => {
-        if (typeof window.scanner === 'undefined') {
-            await new Promise((res, rej) => {
-                const sc = document.createElement('script');
-                sc.onload = res;
-                sc.onerror = rej;
+    const x = () => {
 
-                sc.src = './dist/scanner.js';
-                document.head.appendChild(sc);
-            });
-        }
-
-        window.scanner.init('render');
         window.scanner.start((decodedText, decodedResult) => {
             alert(decodedResult, ',,,', decodedText);
-        });
-
-        document.getElementById('button-scanner-stop').addEventListener('click', () => {
-            window.scanner.stop();
         });
     };
 
     return {
-        init: () => document.getElementById('button-scanner').addEventListener('click', x),
+        init: async () => {
+            if (typeof window.scanner === 'undefined') {
+                await new Promise((res, rej) => {
+                    const sc = document.createElement('script');
+                    sc.onload = res;
+                    sc.onerror = rej;
+
+                    sc.src = './dist/scanner.js';
+                    document.head.appendChild(sc);
+                });
+            }
+
+            window.scanner.init('render');
+
+            document.getElementById('button-scanner').addEventListener('click', x);
+
+            document.getElementById('button-scanner-stop').addEventListener('click', () => {
+                window.scanner.stop();
+            });
+        },
         // start,
         // pause,
     };
