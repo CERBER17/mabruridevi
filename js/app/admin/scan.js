551ff56276e82e72e31b32529439cba3a1ece8c7
dewanakl <dewanakretarta29@gmail.com>
2025-07-19 16:41:46 +0700

    fix: multiple script tag

--- js/app/admin/scan.js
+++ js/app/admin/scan.js
@@ -2,22 +2,24 @@
 export const scan = (() => {
 
     const x = async () => {
-        const scanner = await new Promise((res, rej) => {
-            const sc = document.createElement('script');
-            sc.onload = () => res(window.scanner);
-            sc.onerror = rej;
+        if (typeof window.scanner === 'undefined') {
+            await new Promise((res, rej) => {
+                const sc = document.createElement('script');
+                sc.onload = res;
+                sc.onerror = rej;
 
-            sc.src = './dist/scanner.js';
-            document.head.appendChild(sc);
-        });
+                sc.src = './dist/scanner.js';
+                document.head.appendChild(sc);
+            });
+        }
 
-        scanner.init('scanner');
-        scanner.start((decodedText, decodedResult) => {
+        window.scanner.init('render');
+        window.scanner.start((decodedText, decodedResult) => {
             alert(decodedResult, ',,,', decodedText);
         });
 
         document.getElementById('button-scanner-stop').addEventListener('click', () => {
-            scanner.stop();
+            window.scanner.stop();
         });
     };
 
